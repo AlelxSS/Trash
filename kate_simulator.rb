1b445340d8792ab6cf212d2cb9d7ecf63e4bf977
AlelxSS <titorovichva@gmail.com>
2017-07-07 03:39:58 +0300

    Just adding new file
    Classic girl simulator

---
+++ kate_simulator.rb
@@ -0,0 +1,73 @@
+puts "------------ СИМУЛЯТОР КАТИ ------------"
+
+while (question = gets.chomp.downcase)
+
+	case question
+	when ""
+		break
+	when /привет|здарова/
+		case rand(5)
+		when 4
+			puts "Аагга.."
+		else
+			puts "Привет"
+		end
+	when /почему/
+		case rand(3)
+		when 0
+			puts "Потому"
+		when 1
+			puts "Потому что"
+		else
+			puts "Покочену"
+		end
+	when /зачем/
+		case rand(3)
+		when 0
+			puts "А вот затем"
+		when 1
+			puts "Надо"
+		else
+			puts "За шкафом"
+		end
+	when /что/
+		puts "Что-то"
+	when /кто/
+		puts "Кто-то"
+	when /кого/
+		puts "кого-то"
+	when /где/
+		puts "Где-то"
+	when /как\s|как$/
+		puts "Как-то"
+	when /кому/
+		puts "Кому-то"
+	when /когда/
+		puts "Не знаю"
+	when /какой/
+		puts "Какой-то"
+	when /куда/
+		puts "Куда-то"
+	when /чей/
+		puts "Общий"
+	when /\?/
+		case rand(4)
+		when 0
+			puts "Ой все"
+		when 1
+			puts "Не знаю"
+		when 2
+			puts "Отстань"
+		else
+			puts "...молчание..."
+		end
+	else
+		case rand(4)
+		when 0
+			puts "Аагга.."
+		else
+			puts "...молчание..."
+		end
+	end
+
+end
